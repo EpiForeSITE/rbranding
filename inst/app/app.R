ded09f790c8376f84864456247c224c8f6c60c65
= <william.ray@hsc.utah.edu>
2025-05-16 09:34:57 -0600

    line feed change

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,7 +1,7 @@
 # Returns a filepath to a temporary yaml file
 # brand_dhhs <- function() {load_brand("brand/_brand2.yml", color.main = "blue")}
 
-# ui.page_opts(theme=ui.Theme.from_brand("brand/_gvy_brand.yml"))
+  ui.page_opts(theme=ui.Theme.from_brand("brand/_gvy_brand.yml"))
 
 #' @export
 run_model <- function() {
