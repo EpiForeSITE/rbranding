03fd7648aa819db4cb4f2b564d95da88d9f72f32
= <william.ray@hsc.utah.edu>
2025-05-30 14:29:58 -0600

    make a package, add config file

--- R/get_repo.R
+++ R/get_repo.R
@@ -1,49 +1,76 @@
+#' get the latest branding file from the repository
 
+#' @export
+get_brand <- function() {
+  # This function downloads the latest _brand.yml file from the repository
+  # and compares it with the local _brand.yml file.
+  # If they are different, it prompts the user to overwrite the local file.
+  # If the local file does not exist, it will be created.
 
+  # load the config file
+  # if you want to use a different config file, you can uncomment the line below
+  # and provide the path to your config file.
 
-
-
-
-remote_file = "https://raw.githubusercontent.com/EpiForeSITE/branding-package/refs/heads/main/_brand.yml"
-local_file = "_brand.yml"
-
-
-tempfile_name = tempfile()
-# add exception handling if the download fails
-tryCatch(
-  {
-    download.file(
-      remote_file,
-      destfile = tempfile_name)
-  },
-  error = function(e) {
-    print(paste("Error downloading file:", e))
+  # read a yaml file with the remote and local file paths
+  read_yaml <- function(file) {
+    if (!file.exists(file)) {
+      stop(paste("File not found:", file))
+    }
+    yaml::yaml.load_file(file)
   }
-)
-
-download.file(
-            remote_file,
-            destfile = tempfile_name)
-temp_hash = tools::md5sum(tempfile_name)
-local_hash = tools::md5sum(local_file)
+  config <- read_yaml("config.yml")
+  remote_file <- config$remote_file
+  local_file <- config$local_file
 
 
-print(paste("local hash equals remote hash: ", (local_hash == temp_hash)))
-# prompt user to overwrite if the hashes are not equal.
-# add exception handling is the file is not found
 
-if (local_hash != temp_hash) {
-  print("The local file is different from the remote file.")
-  print("do you want to overwrite it? (y/n)")
-  answer <- readline()
-  if (answer == "y") {
-    file.copy(tempfile_name, "_brand.yml", overwrite = TRUE)
-    print("File overwritten.")
+
+  tempfile_name <- tempfile()
+  # add exception handling if the download fails
+  tryCatch(
+    {
+      message("Checking remote version... ")
+      download.file(
+        remote_file,
+        destfile = tempfile_name,
+        quiet = TRUE
+      )
+    },
+    error = function(e) {
+      message(paste("Error downloading file:", e))
+    }
+  )
+
+
+  temp_hash <- tools::md5sum(tempfile_name)
+  local_hash <- tools::md5sum(local_file)
+
+
+
+  # prompt user to overwrite if the hashes are not equal.
+  # add exception handling is the file is not found
+  if (local_hash != temp_hash) {
+    message("The local file is different from the remote file.")
+    message("1: Overwrite the local file with the remote file")
+    message("2: Overwrite the local file with the remote file and save a backup to bak_brand.yml")
+    message("3: Do nothing")
+    message("\nPlease Select an option: (1/2/3): ")
+    answer <- readline()
+    if (answer == "1") {
+      file.copy(tempfile_name, "_brand.yml", overwrite = TRUE)
+      message("_brand.yml replaced with latest from repository.")
+    }
+    if (answer == "2") {
+      file.copy("_brand.yml", "bak_brand.yml", overwrite = TRUE)
+      file.copy(tempfile_name, "_brand.yml", overwrite = TRUE)
+      message("_brand.yml replaced with latest from repository.")
+      message("backup saved to bak_brand.yml")
+    }
+    if (answer == "3") {
+      message("No action taken.")
+    }
+    #' add validation that answer is in 1,2,3
   } else {
-    print("File not overwritten.")
+    message("The local file is the same as the remote file. No action taken.")
   }
-} else {
-  print("The local file is the same as the remote file. No action taken.")
 }
-
-
