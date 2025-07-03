eb0cbd5999abf8f66e16d04ad5ad8789bff20a12
Your Name <your.email@example.com>
2025-07-03 12:46:59 -0600

    fix minor issues with documentation

--- R/get_template.R
+++ R/get_template.R
@@ -1,43 +1,9 @@
-#' Copy Example App File to User Directory
-#'
-#' This function creates the folders `inst/app` in the user's working directory and copies the example file `link_plots.R` into that folder.
-#' @export
-get_template2 <- function() {
-  target_dir <- file.path(getwd(), "inst", "app")
-  if (!dir.exists(target_dir)) {
-    dir.create(target_dir, recursive = TRUE)
-  }
-  example_file <- system.file("examples/shiny2", "app.R", package = "rbranding")
-  if (example_file == "") {
-    stop("Example file not found in the package.")
-  }
-  file.copy(example_file, file.path(target_dir, "app.R"), overwrite = TRUE)
-  message("Example file copied to ", file.path(target_dir, "link_plots.R"))
-
-  # Repeat for server.R
-  example_file_server <- system.file("examples/shiny2", "server.R", package = "rbranding")
-  if (example_file_server == "") {
-    stop("server.R file not found in the package.")
-  }
-  file.copy(example_file_server, file.path(target_dir, "server.R"), overwrite = TRUE)
-  message("server.R file copied to ", file.path(target_dir, "server.R"))
-
-
-
-  # Repeat for server.R
-  example_file_server <- system.file("examples/shiny2", "ui.R", package = "rbranding")
-  if (example_file_server == "") {
-    stop("ui.R file not found in the package.")
-  }
-  file.copy(example_file_server, file.path(target_dir, "ui.R"), overwrite = TRUE)
-  message("server.R file copied to ", file.path(target_dir, "server.R"))
-}
-
+#' @title Get Shiny Templates
 #' Copy Example App Files from inst/examples to inst/app
 #'
 #' @param example_name Name of the example folder under inst/examples
 #' @export
-get_example <- function(example_name) {
+get_template <- function(example_name) {
   # Find the source directory inside the package
   source_dir <- system.file("examples", example_name, package = "rbranding")
   if (source_dir == "") {
