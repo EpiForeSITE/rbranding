a48e43e5ee064852e4eb8d4193aa4d5262c1f876
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-09-30 12:57:23 -0600

    Fix setting GHA_TEST variable

--- tests/manual/github-get-brand.R
+++ tests/manual/github-get-brand.R
@@ -2,7 +2,7 @@ library(rbranding)
 library(yaml)
 
 # Set GHA_TEST mode
-Sys.setenv(GHA_TEST = "true")
+Sys.setenv(GHA_TEST = TRUE)
 
 # Function to run get_brand() with simulated user input
 run_get_brand_with_input <- function() {
